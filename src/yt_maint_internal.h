be088ab1af432ef012b0145528ba6e770f2e6bb7
Stephen Hurd <deuce@synchro.net>
2026-09-16 13:22:37 -0400

    Split maintenance player phase

--- src/yt_maint_internal.h
+++ src/yt_maint_internal.h
@@ -3,7 +3,24 @@
 
 #include "yt_maint.h"
 
+struct maint_state {
+	struct yt_game game;
+	float *player_sector;
+	float *player_cloak;
+	int player_count;
+	int sector_count;
+	int port_count;
+	int planet_count;
+	int today;
+	struct yt_maintenance_route_cache route_cache;
+};
+
 bool maintenance_copy_part(uint8_t *dest, size_t capacity, size_t *length,
     const uint8_t *data, size_t data_length);
+bool yt_maintenance_remove_player_from_teams(struct maint_state *state,
+    int player_record, struct yt_error *error);
+bool yt_maintenance_players_run(struct maint_state *state,
+    yt_maintenance_score_line_fn line_output, void *line_context,
+    struct yt_error *error);
 
 #endif
