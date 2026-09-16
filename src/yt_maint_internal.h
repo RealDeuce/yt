ec18763533c353a42d2f8ef467c213891fba6ea5
Stephen Hurd <deuce@synchro.net>
2026-09-16 13:33:10 -0400

    Split maintenance Xannor phase

--- src/yt_maint_internal.h
+++ src/yt_maint_internal.h
@@ -31,5 +31,14 @@ bool yt_maintenance_remove_player_from_teams(struct maint_state *state,
 bool yt_maintenance_players_run(struct maint_state *state,
     yt_maintenance_score_line_fn line_output, void *line_context,
     struct yt_error *error);
+bool maintenance_emit_output_row(
+    const struct yt_maintenance_output_result *output, uint16_t address,
+    yt_maintenance_score_line_fn line_output, void *line_context,
+    struct yt_error *error);
+bool maintenance_stdout_line(void *context, const uint8_t *line,
+    size_t length, struct yt_error *error);
+bool yt_maintenance_xannor_run(struct maint_state *state,
+    yt_maintenance_score_line_fn line_output, void *line_context,
+    struct yt_error *error);
 
 #endif
